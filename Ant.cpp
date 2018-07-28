653b9d9950c4466b9bb15cb56bbfbe3a9f224dc7
JayBreak <Jakob.we.wii@gmail.com>
2018-07-28 13:14:25 -0700

    changed move and valid_move

--- Ant.cpp
+++ Ant.cpp
@@ -9,21 +9,23 @@ Ant::Ant() {
 	col = 1;
 }
 
-<<<<<<< HEAD
-bool Ant::valid_move(char** grid, Ant* ants) {
-
-=======
-bool Ant::valid_move(critter* critters, int direction) {
+bool Ant::valid_move(char** grid,Ant* ant, int direction) {
 
 	if (random == 0)
 	{
 		for(int i = 0; i <)
 	}
->>>>>>> 073524cfb1ef8d014ec3bc31e52efed2cc6751d9
 }
 
 char** Ant::move(char** grid, Ant* ants) {
-	int random = rand() % 4;  // 0 up, 1 right, 2 down, 3 left.
+	int direction = rand() % 4;  // 0 up, 1 right, 2 down, 3 left.
+	if(valid_move(gird,ants,direction))
+	{
+
+	}
+
+
+	return grid;
 
 }
 
